c908e5812e05c2dd23cdc06fc792c0036128e770
Patric Ronge <pronge@gmail.com>
2020-04-09 17:56:07 +0200

    finished header (placerholder colors)

--- header.php
+++ header.php
@@ -8,6 +8,30 @@
   </head>
   <body>
     <header class="header">
+      <nav class="header__hidden-burgernav">
+        <input
+          type="text"
+          class="header__search"
+          placeholder="Search Product"
+        />
+        <ul class="header__list">
+          <a href="" class="header__item-link"
+            ><li class="header__list-item">Home</li></a
+          >
+          <a href="" class="header__item-link"
+            ><li class="header__list-item">Sneakers</li></a
+          >
+          <a href="" class="header__item-link"
+            ><li class="header__list-item">Boots</li></a
+          >
+          <a href="" class="header__item-link"
+            ><li class="header__list-item">Dress</li></a
+          >
+          <a href="" class="header__item-link"
+            ><li class="header__list-item">Running</li></a
+          >
+        </ul>
+      </nav>
       <div class="header__nav">
         <span class="header__nav-line"></span>
       </div>
