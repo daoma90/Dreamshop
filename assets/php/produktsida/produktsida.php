96c6d0404bac3fc2a378bdddd5eb80f9abda7b7b
Amin Titi <titiamin@icloud.com>
2020-04-10 20:58:02 +0200

    pictur

---
+++ assets/php/produktsida/produktsida.php
@@ -0,0 +1,67 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style/productsida.css">
+    <script src="https://kit.fontawesome.com/e160d99317.js" crossorigin="anonymous"></script>
+    <title>ProduktSida</title>
+</head>
+<body>
+
+<nav>
+<i class="fas fa-align-justify"></i>
+<span class="NavSpan">Dream Team</span>
+<i class="fas fa-shopping-cart"></i>
+</nav>
+<main>
+    <div class= wrapper>
+        <div class="imgDIv">
+            <img src="../../media/rmwilliams.jpg" alt="rmwilliams">
+        </div>
+        
+        <div  class="subWrapper">
+        <div class="subImgDiv">
+        <img src="../../media/rmwilliams.jpg" alt="rmwilliams">
+        </div>
+        <div class="subImgDiv">
+        <img src="../../media/rmwilliams.jpg" alt="rmwilliams">
+        </div>
+        <div class="subImgDiv">
+        <img src="../../media/rmwilliams.jpg" alt="rmwilliams">
+        </div>
+        <div class="subImgDiv">
+        <img src="../../media/rmwilliams.jpg" alt="rmwilliams">
+        </div>
+        <div class="subImgDiv">
+        <img src="../../media/rmwilliams.jpg" alt="rmwilliams">
+        </div>
+       </div>
+
+
+       <div class="productInfo">
+        <h2 class="productName">RM:Williams</h2>
+        <span>4999</span>
+        <input type="number" class="size">
+        <p>  A classic style from Australian R.M. Williams, a brand known for their high-quality footwear,
+             well-constructed and durable for both work and leisure.
+             These boots have an upper in premium suede and elastic gussets at both sides. The outsole has a sewn welt construction.</p>
+        
+        <div class="btns">
+        <i class="fas fa-plus"></i>
+        <input type="number" class="input">
+        <i class="fas fa-minus"></i>
+      
+       </div>
+       <button class="buyBtn">Buy</button>
+      </div>
+       </div>
+
+       
+
+    
+</main>
+</body>
+</html>
