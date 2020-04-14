725a3472c90e0c8a211a8c7db78a51277a9423f8
ramy <ramy.niranjan@gmail.com>
2020-04-14 16:43:58 +0200

    finish up update function & create a function for deleting image

--- admin/assets/php/productCurd.php
+++ admin/assets/php/productCurd.php
@@ -26,7 +26,7 @@ function deleteProduct($pdo)
 
 function createProduct($pdo)
 {
-  //  and !empty($fileName)
+
   if (isset($_POST['addProduct'])) {
 
     $targetDir = "../images";
@@ -77,7 +77,7 @@ function createProduct($pdo)
 function fillingEditForm($pdo)
 {
 
-  $id = $_POST['val'];
+  $id = $_POST['val']; // gett the id where need be updated
   $sql = 'SELECT * FROM products WHERE id=:id';
   $stmt = $pdo->prepare($sql);
   $stmt->execute([
@@ -85,35 +85,74 @@ function fillingEditForm($pdo)
   ]);
   $product = $stmt->fetch(PDO::FETCH_ASSOC);
   $product_info = array();
-  array_push($user_info, $product['id'], $product['name'], $product['description'], $product['price'], $product['image'], $product['cat_id']);
+  array_push(
+    $product_info,
+    $product['id'],
+    $product['name'],
+    $product['description'],
+    $product['price'],
+    $product['image'], //$imageNameFromDB
+    $product['featured'],
+    $product['in_stock'],
+    $product['cat_id']
+  );
+  //when you fill data take the image name assign to img tag to get image preview
   return $product_info;
 }
 
-function updateProduct($pdo)
+function updateProduct($pdo, $imageNameFromDB)
 {
-  if (isset($_POST['edit'])) {
-    $id = $_POST['update-id']; //getting id for update
+  if (isset($_POST['editProduct'])) {
+
+    $targetDir = "../images";
+    $fileName = $_FILES["file"]["name"];
+    $temp_name  = $_FILES['file']['tmp_name'];
+    $targetFilePath = $targetDir . $fileName;
+
+
     $name = trim($_POST['name']);
     $description = trim($_POST['description']);
     $price = trim($_POST['price']);
-    $image = trim($_POST['image']);
     $cat_id = trim($_POST['cat_id']);
+    $featured = trim($_POST['featured']);
+    $in_stock = trim($_POST['in_stock']);
 
-    if (empty($name) || empty($description) || empty($price) || empty($image) || empty($cat_id)) {
-      echo 'error msg'; //not finalize
+    if (isset($fileName) and !empty($fileName)) {
+
+      if (move_uploaded_file($temp_name, $targetFilePath)) {
+
+
+        $sql = 'UPDATE products SET name=:name,description=:description,price=:price,image=:fileName,featured=:featured,in_stock=:in_stock,cat_id=:cat_id WHERE id=:id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+          ':name' => $name,
+          ':description' => $description,
+          ':price' => $price,
+          ':fileName' => $fileName,
+          ':featured' => $featured,
+          ':in_stock' => $in_stock,
+          ':cat_id' => $cat_id,
+
+        ]);
+        deletingImageNotInUse($fileName, $imageNameFromDB); //deleting image that not in use
+        header('Location:index.php');
+      } else {
+        echo  "Sorry, there was an error uploading your file.";
+      }
     } else {
-      $sql = 'UPDATE products SET name=:name,email=:email,description=:description,price=:price,image=:image,cat_id=:cat_id WHERE id=:id';
-      $stmt = $pdo->prepare($sql);
-      $stmt->execute([
-        'id' => $id,
-        ':name' => $name,
-        ':description' => $description,
-        ':price' => $price,
-        ':image' => $image,
-        ':cat_id' => $cat_id,
-
-      ]);
-      header('Location:index.php');
+      echo 'You should select a file to upload !!';
     }
+  } else {
+    echo 'You cant leave field empty';
+  }
+}
+
+
+function deletingImageNotInUse($fileName, $imageNameFromDB)
+{
+  if ($fileName != $imageNameFromDB) {
+
+    $file_to_delete = '../images/' . $imageNameFromDB;
+    unlink($file_to_delete);
   }
 }
