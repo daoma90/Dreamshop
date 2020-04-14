ace7a59d9347b88e7b97cb95efa1d93455987cb7
David Öman <Daoma90@gmail.com>
2020-04-14 11:49:34 +0200

    Added header and changed file structure

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,22 @@
-// include curd & pdo files
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Frontendprojekt</title>
+        <link rel="stylesheet" href="./assets/style/main.css">
+        <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+    </head>
+    <body>
+        <?php require_once "header.php";?>
+
+        <div class="content">
+            <h1 class="content-here">Content här</h1>
+        </div>
+
+        <script src="./assets/js/header.js"></script>
+    </body>
+</html>
