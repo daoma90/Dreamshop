7cdd51758bfe933d1ebc03ab03a70b228095d463
Samuel Mårtensson <samuel137@live.se>
2020-04-16 11:31:19 +0200

    prepare for live hosting

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,10 +1,15 @@
 <?php
-
-$dsn = "mysql:host=localhost;dbname=webshop_cms";
-try {
-  $pdo = new PDO($dsn, 'ramy', 'test12345');
-  //username & password should change accordingly
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  echo $e->getMessage();
-}
+    $db_server = "localhost";
+    $db_database = "jlwvfkou_wp888";
+    $db_username = "jlwvfkou_wp888";
+    $db_password = 'JSU8Sp83[!';
+    try{
+    $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8"
+    ,
+    $db_username, $db_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e){
+    echo $e-> getMessage();
+    }
+?>
