81e9516df4a831e23a1348ee6e74710935ad025e
Samuel Mårtensson <samuel137@live.se>
2020-04-16 14:10:11 +0200

    updated db for hosting

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=frontendproject";
+$dsn = "mysql:host=localhost;dbname=jlwvfkou_wp888";
 //$dsn = "mysql:host=localhost;dbname=webshop_cms";
 try {
-  $pdo = new PDO($dsn, 'root', 'root');
+  $pdo = new PDO($dsn, 'jlwvfkou_wp888', 'JSU8Sp83[!');
   //username & password should change accordingly
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
