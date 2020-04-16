f464fe289cdfc3501d0f9c586b1a142e2149d799
ramy <ramy.niranjan@gmail.com>
2020-04-16 10:20:02 +0200

    fix minor bug

--- admin/category/categoriesUpdate.php
+++ admin/category/categoriesUpdate.php
@@ -1,11 +1,11 @@
 <?php
-require_once './assets/php/db.php';
+require_once '../includes/db.php';
 if (isset($_POST['updateCat'])) {
     //Save if new image is uploaded
     $imageName = $_FILES['image']['name'];
     $imageError = $_FILES['image']['error'];
     $imageTemp = $_FILES['image']['tmp_name'];
-    $imagePath = "./assets/media/";
+    $imagePath = "../images/";
 
     if(is_uploaded_file($imageTemp)) {
         move_uploaded_file($imageTemp, $imagePath . $imageName);
@@ -44,4 +44,3 @@ if (isset($_POST['updateCat'])) {
     }
     header('Location:Index.php');
   }
-?>
--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=frontend_project";
+$dsn = "mysql:host=localhost;dbname=webshop_cms";
 //$dsn = "mysql:host=localhost;dbname=webshop_cms";
 try {
-  $pdo = new PDO($dsn, 'root', '');
+  $pdo = new PDO($dsn, 'ramy', 'test12345');
   //username & password should change accordingly
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
