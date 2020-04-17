7e6be6e5c054428e1123916f70b345d3cabbe940
Samuel Mårtensson <samuel137@live.se>
2020-04-17 10:55:44 +0200

    fixed category sort classes

--- assets/php/footer_categories.php
+++ assets/php/footer_categories.php
@@ -11,10 +11,10 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
     $name = $row['name'];
 
     if (strpos($_SERVER['REQUEST_URI'], 'product')){
-        echo "<li class='footer__list-item catsort-footer'><a class='footer__item-link' href='./?category=$name&#category'>$name</a></li>";
+        echo "<li class='footer__list-item catsort'><a class='footer__item-link' href='./?category=$name&#category'>$name</a></li>";
     }
     else {
-        echo "<li class='footer__list-item catsort-footer'>$name</li>";
+        echo "<li class='footer__list-item catsort'>$name</li>";
  }
 endwhile;
 
--- assets/php/header_categories.php
+++ assets/php/header_categories.php
@@ -11,7 +11,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
     $name = $row['name'];
 
     if (strpos($_SERVER['REQUEST_URI'], 'product')){
-        echo "<li class='header__list-item catsort-header'><a class='header__item-link' href='./?category=$name&#category'>$name</a></li>";
+        echo "<li class='header__list-item catsort'><a class='header__item-link' href='./?category=$name&#category'>$name</a></li>";
     }
     else {
         echo "<li class='header__list-item catsort-header'>$name</li>";
