eea6bd8623bb4c2bbc37b4490cce37cbf0834482
Samuel Mårtensson <samuel137@live.se>
2020-04-17 11:56:57 +0200

    added charset to admin db

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=jlwvfkou_wp888";
+$dsn = "mysql:host=localhost;dbname=jlwvfkou_wp888;charset=utf8";
 try {
   $pdo = new PDO($dsn, 'jlwvfkou_wp888', 'JSU8Sp83[!');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
