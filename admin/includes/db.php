8d8692e6776c9ae0884041c132c7fed5b2dd6a7a
Samuel Mårtensson <samuel137@live.se>
2020-04-17 11:26:09 +0200

    fixed burger in safari?

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=jlwvfkou_wp888";
+$dsn = "mysql:host=localhost;dbname=frontendproject";
 try {
-  $pdo = new PDO($dsn, 'jlwvfkou_wp888', 'JSU8Sp83[!');
+  $pdo = new PDO($dsn, 'root', 'root');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo $e->getMessage();
