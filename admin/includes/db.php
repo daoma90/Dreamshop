a9f6d048395a69a9578d201d9fcdc25ddf45ad30
ramy <ramy.niranjan@gmail.com>
2020-04-17 20:34:34 +0200

    chang my db

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=jlwvfkou_wp888;charset=utf8";
+$dsn = "mysql:host=localhost;dbname=webshop_cms;charset=utf8";
 try {
-  $pdo = new PDO($dsn, 'jlwvfkou_wp888', 'JSU8Sp83[!');
+  $pdo = new PDO($dsn, 'ramy', 'test12345');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo $e->getMessage();
--- assets/php/db.php
+++ assets/php/db.php
@@ -1,15 +1,9 @@
 <?php
-    $db_server = "localhost";
-    $db_database = "jlwvfkou_wp888";
-    $db_username = "jlwvfkou_wp888";
-    $db_password = 'JSU8Sp83[!';
-    try{
-    $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8"
-    ,
-    $db_username, $db_password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e){
-    echo $e-> getMessage();
-    }
-?>
+
+$dsn = "mysql:host=localhost;dbname=webshop_cms;charset=utf8";
+try {
+    $pdo = new PDO($dsn, 'ramy', 'test12345');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
