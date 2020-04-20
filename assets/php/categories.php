1a5527e63ad89d3c9938c5b2cba8c4efff5bcad3
Samuel Mårtensson <samuel137@live.se>
2020-04-20 11:10:46 +0200

    created a seperate page for each category

--- assets/php/categories.php
+++ assets/php/categories.php
@@ -11,8 +11,9 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
     $name = $row['name'];
     $image = $row['image'];
 echo "<article class='lp-categories__item'>
+        <a  href='./category.php?category=$name'>
         <img class='lp-categories__img' src='./admin/images/$image' alt=''>
-        <div class='lp-categories__title'>$name</div>
+        <div class='lp-categories__title'>$name</div></a>
       </article>";
 endwhile;
 
