951ac28394fca9ca2f98bb55de1ea764020f9e62
Patric Ronge <pronge@gmail.com>
2020-04-20 12:49:21 +0200

    fixed img contain and horizontal padding on prods

--- assets/php/db.php
+++ assets/php/db.php
@@ -1,6 +1,6 @@
 <?php
     $db_server = "localhost";
-    $db_database = "jlwvfkou_wp888";
+    $db_database = "frontendproject";
     $db_username = "jlwvfkou_wp888";
     $db_password = 'JSU8Sp83[!';
     try{
