09e55537553c8bbe3879507dcaeed15f1251716b
Amin Titi <titiamin@icloud.com>
2020-04-21 21:57:43 +0200

    lastuppdate

--- assets/js/confirmation.js
+++ assets/js/confirmation.js
@@ -1,12 +1,15 @@
 for (var i = 0; i < localStorage.length; i++) {
+    let key = localStorage.getItem(localStorage.key(i))
+    let value = JSON.parse(key)
 
-  let key = localStorage.getItem(localStorage.key(i))
-  let value = JSON.parse(key)
-  let arr = []
+    for (let i = 0; i < value.length; i++) {
+        const elm = value[i]
+        populate(elm)
+    }
+}
 
-  for (let i = 0; i < value.length; i++) {
-    const elm = value[i];
-
+function populate(elm) {
+    let arr = []
     let price = parseInt(elm.price)
     let totalSumma = elm.quantity * price
     arr.push(totalSumma)
@@ -19,20 +22,13 @@ for (var i = 0; i < localStorage.length; i++) {
     <div> <img src="../FE-Project-Shop/admin/images/${elm.image}">
     <div> Totalsumma: ${CountSum(arr)}</div>
     </div>`
-
-
-
-    //TOtal Summa 
-
-  }
-
 }
 
 function CountSum(totalsumma) {
-  let sum = 0
-  for (let i = 0; i < totalsumma.length; i++) {
-    let elm = totalsumma[i]
-    sum += elm
-  }
-  return sum
+    let sum = 0
+    for (let i = 0; i < totalsumma.length; i++) {
+        let elm = totalsumma[i]
+        sum += elm
+    }
+    return sum
 }
