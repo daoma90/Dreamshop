db8d2bdb3f0826503832024746c4bfea0e1a1f26
Samuel Mårtensson <samuel137@live.se>
2020-04-21 09:59:08 +0200

    updated css for orders and slightly changed header behaviour between devices

--- admin/order/orders.php
+++ admin/order/orders.php
@@ -5,19 +5,20 @@
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 </head>
-<body>
+<body class ="orders-bg">
     <?php
     require_once "../includes/db.php";
     require_once "../includes/header.php";
     ?>
     <section class="order-wrap">
     <h1 class="order-wrap__heading">Orders:</h1>
-
+    <div class="order-wrap__table-wrapper">
     <table class="orders">
         <tr>
             <th>Order ID</th>
-            <th>Status</th> 
             <th>Date</th>
+            <th>Customer ID</th>
+            <th>Status</th>
         </tr>
         <?php
 
@@ -31,6 +32,7 @@
             $orderID = htmlspecialchars($row["ID"]);
             $status = htmlspecialchars($row["status"]);
             $date = htmlspecialchars($row['date']);
+            $customer = htmlspecialchars($row['customer_id']);
 
             $selected = "<select onchange='updateOrder($orderID, event)'>
                             <option " . ($status == "new" ? "selected" : NULL) . " value='new'>New</option>
@@ -42,8 +44,9 @@
                         <tr>
                             <td>$orderID</td>
                             <input type='hidden' name='id' value='$orderID'>
-                            <td>$selected</td>
                             <td>$date</td>
+                            <td>$customer</td>
+                            <td>$selected</td>
                         </tr>
                     ";
         }
@@ -55,8 +58,9 @@
     <table class="orders">
             <tr>
                 <th>Order ID</th>
-                <th>Status</th> 
                 <th>Date</th>
+                <th>Customer ID</th>
+                <th>Status</th> 
             </tr>
             <?php
 
@@ -70,6 +74,7 @@
                 $orderID = htmlspecialchars($row["ID"]);
                 $status = htmlspecialchars($row["status"]);
                 $date = htmlspecialchars($row['date']);
+                $customer = htmlspecialchars($row['customer_id']);
     
                 $selected = "<select onchange='updateOrder($orderID, event)'>
                                 <option " . ($status == "new" ? "selected" : NULL) . " value='new'>New</option>
@@ -81,14 +86,16 @@
                             <tr>
                                 <td>$orderID</td>
                                 <input type='hidden' name='id' value='$orderID'>
-                                <td>$selected</td>
                                 <td>$date</td>
+                                <td>$customer</td>
+                                <td>$selected</td>
                             </tr>
                         ";
             }
             $orders .= "</table>";
             echo $orders;
             ?>
+            </div>
         </section>
 
     <script src="../js/header.js"></script>
