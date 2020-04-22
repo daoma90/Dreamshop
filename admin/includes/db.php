33785d193535ed95e24f043ddb5ec0c4146ab1af
Christopher Berge <chrberge88@gmail.com>
2020-04-22 12:40:50 +0200

    fixed eventlistener header js issue

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=frontendproject;charset=utf8";
+$dsn = "mysql:host=localhost;dbname=jlwvfkou_wp888;charset=utf8";
 try {
-  $pdo = new PDO($dsn, 'root', '');
+  $pdo = new PDO($dsn, 'jlwvfkou_wp888', 'JSU8Sp83[!');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo $e->getMessage();
