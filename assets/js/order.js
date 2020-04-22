b1dcf71c82a51a06011f37cc0b638448483461c2
Patric Ronge <pronge@gmail.com>
2020-04-22 18:16:07 +0200

    minor fix for price container

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,5 +1,5 @@
 function removeItem(e) {
-  const item = e.target.parentElement.querySelector('.products__name')
+  const item = e.target.parentElement.querySelector(".products__name")
     .textContent;
 
   const productInCart = HappyLib.findProduct(item, cart);
@@ -12,8 +12,8 @@ function removeItem(e) {
 }
 
 function changeQuantity(e) {
-  const val = e.target.parentElement.querySelector('.products__qty');
-  const item = e.target.parentElement.querySelector('.products__name')
+  const val = e.target.parentElement.querySelector(".products__qty");
+  const item = e.target.parentElement.querySelector(".products__name")
     .textContent;
 
   const productInCart = HappyLib.findProduct(item, cart);
@@ -27,47 +27,48 @@ function changeQuantity(e) {
 }
 
 function handleProductQty(e) {
-  const clickedProduct = e.target.parentElement.querySelector('.products__name')
+  const clickedProduct = e.target.parentElement.querySelector(".products__name")
     .textContent;
-  const inputQty = e.target.parentElement.querySelector('.products__qty');
+  const inputQty = e.target.parentElement.querySelector(".products__qty");
   const productInCart = HappyLib.findProduct(clickedProduct, cart);
   if (productInCart !== undefined && inputQty.value > productInCart.stock) {
     productInCart.quantity = productInCart.stock;
     HappyLib.updateLocalStorage(cart.key, renderProducts);
-    alert('Stock limit reached');
+    alert("Stock limit reached");
   }
 }
 
 function updatePrice(str) {
-  const totalPrice = document.querySelector('.products__total');
-  const shipping = document.querySelector('.products__shipping-price');
-  const headline = document.querySelector('.products__headline');
-  const priceExclShipping = document.querySelector('.products__price-price');
+  const totalPrice = document.querySelector(".products__total");
+  const shipping = document.querySelector(".products__shipping-price");
+  const headline = document.querySelector(".products__headline");
+  const priceExclShipping = document.querySelector(".products__price-price");
   const price = HappyLib.getTotalPrice(cart.products);
 
   if (str) str = str.toLowerCase();
 
   if (cart.products === undefined || cart.products.length === 0) {
-    headline.textContent = 'Your cart is empty';
-    shipping.textContent = '0';
-    totalPrice.textContent = '0';
+    headline.textContent = "Your cart is empty";
+    shipping.textContent = "0";
+    totalPrice.textContent = "0";
+    priceExclShipping.textContent = "0";
   } else {
-    headline.textContent = 'Your Order';
-    priceExclShipping.textContent = price + ' SEK';
-    if (price > 500 || str === 'stockholm') {
-      shipping.textContent = 'FREE';
-      totalPrice.textContent = price + ' SEK';
+    headline.textContent = "Your Order";
+    priceExclShipping.textContent = price + " SEK";
+    if (price > 500 || str === "stockholm") {
+      shipping.textContent = "FREE";
+      totalPrice.textContent = price + " SEK";
     } else {
-      shipping.textContent = '50 SEK';
-      totalPrice.textContent = price + 50 + ' SEK';
+      shipping.textContent = "50 SEK";
+      totalPrice.textContent = price + 50 + " SEK";
     }
   }
 }
 
 function renderProducts() {
-  const items = document.querySelector('.products__container');
+  const items = document.querySelector(".products__container");
 
-  items.innerHTML = '';
+  items.innerHTML = "";
   renderCart();
   cart.products.forEach(function (item) {
     items.innerHTML += `<div class="products__item">
@@ -84,12 +85,12 @@ function renderProducts() {
     
     </div>`;
   });
-  const removeBtn = document.querySelectorAll('.products__remove-btn');
-  const qtyInput = document.querySelectorAll('.products__qty');
+  const removeBtn = document.querySelectorAll(".products__remove-btn");
+  const qtyInput = document.querySelectorAll(".products__qty");
 
-  HappyLib.addEvents(removeBtn, removeItem, 'click');
-  HappyLib.addEvents(qtyInput, changeQuantity, 'change');
-  HappyLib.addEvents(qtyInput, handleProductQty, 'change');
+  HappyLib.addEvents(removeBtn, removeItem, "click");
+  HappyLib.addEvents(qtyInput, changeQuantity, "change");
+  HappyLib.addEvents(qtyInput, handleProductQty, "change");
 
   updatePrice();
 }
@@ -97,12 +98,12 @@ function renderProducts() {
 HappyLib.localStorageInit(cart.key);
 renderProducts();
 const cityInput = addOrder.querySelector('input[name="city"]');
-cityInput.addEventListener('keyup', function () {
+cityInput.addEventListener("keyup", function () {
   updatePrice(this.value);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('.form-wrapper__form');
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".form-wrapper__form");
   cart.products.forEach(function (product) {
     const template = `<input type="hidden" name="products[]" value="${product.id}" /><input type="hidden" name="quantity[]" value="${product.quantity}" />`;
     form.innerHTML += template;
