a5ff18987783291855698436ef55f1ffcf92d74c
Christopher Berge <chrberge88@gmail.com>
2020-04-23 14:25:31 +0200

    fixed pdo variable

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -6,7 +6,7 @@ $db_username = "jlwvfkou_wp888";
 $db_password = 'JSU8Sp83[!'; 
 
 try{
-$db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8",
+$pdo = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8",
 $db_username, $db_password);
 
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
