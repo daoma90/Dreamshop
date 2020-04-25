c2043200c9da8c28c4fc5f17bb8db4c65491a8f0
David Öman <Daoma90@gmail.com>
2020-04-25 16:51:31 +0200

    Fixed product divider line on confirmation page Fixed order id

--- assets/js/confirmation.js
+++ assets/js/confirmation.js
@@ -1,8 +1,10 @@
-const total = document.getElementById('total').dataset.total;
-const productsContainer = document.querySelector('.confirmation-products');
+const total = document.getElementById("total").dataset.total;
+const productsContainer = document.querySelector(".confirmation-products");
 
-const id = document.getElementById('id').dataset.id;
-const productsTop = document.querySelector('.products-top');
+const id = document.getElementById("id").dataset.id;
+const productsTop = document.querySelector(".products-top");
+
+productsTop.innerHTML += "<p class='products-id'>Order ID: " + id + "</p>";
 
 for (var i = 0; i < localStorage.length; i++) {
   let key = localStorage.getItem(localStorage.key(i));
@@ -10,7 +12,7 @@ for (var i = 0; i < localStorage.length; i++) {
 
   for (let i = 0; i < value.length; i++) {
     const elm = value[i];
-    let wrapper = document.querySelector('.confirmation-products');
+    let wrapper = document.querySelector(".confirmation-products");
     wrapper.innerHTML += `
     <div class="product">
       <div class="product__image"> <img src="../FE-Project-Shop/admin/images/${elm.image}"></div>
@@ -30,13 +32,12 @@ for (var i = 0; i < localStorage.length; i++) {
       .concat(elm.image, '"></div>'); */
   }
 }
-productsTop.innerHTML += "<p class='products-id'>Order ID: " + id + '</p>';
 productsContainer.innerHTML +=
   '<div class="confirmation-products__total"><p class="confirmation-products__total-text">TOTAL</p>' +
   '<p class="confirmation-products__total-number">' +
   total +
-  ' €' +
-  '</p></div>';
+  " €" +
+  "</p></div>";
 localStorage.clear();
 
 /* let wrapper = document.querySelector(".confirmation-products");
