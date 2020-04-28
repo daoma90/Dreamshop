93bc9d1c6d8258d16ec913d97dbcda2beed00771
Samuel Mårtensson <samuel137@live.se>
2020-04-28 18:15:18 +0200

    added dynamic titles to each page on frontend

--- oldOut.php
+++ oldOut.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Frontendprojekt</title>
+    <title>On sale - DreamShop</title>
     <link rel="stylesheet" href="./assets/style/main.css">
     <link
       rel="stylesheet"
--- product.php
+++ product.php
@@ -49,7 +49,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Frontendprojekt</title>
+    <title><?= $name . ' - DreamShop' ?></title>
     <link rel="stylesheet" href="./assets/style/main.css" />
     <link
       rel="stylesheet"
