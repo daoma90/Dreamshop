1068f3e062387c80b7a492f9c0e4969407cfe45c
ramy <ramy.niranjan@gmail.com>
2020-04-29 10:13:28 +0200

    moved validation to js from php in searchprod.php

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,2 +1,15 @@
 let header = document.querySelector('.header')
 header.style.position='static'
+
+
+const words_to_filter = ["and", "not", "if", "get", "with", "keep", "that", "this", "every", "is", 'let', 'go'];
+function checkForm(form){
+  let search = form.searchWord.value
+  if (search.length <= 1 || words_to_filter.includes(search.trim()) ){
+    // need fix modal for better user experince
+    alert("Error: Input can't be empty or atleast tow character or can't be key word");
+    return false;
+  }
+  return true
+ 
+}
--- searchProd.php
+++ searchProd.php
@@ -19,57 +19,54 @@
 
 
 
-    <?php
+  <?php
 
-    $words_to_filter = array("and", "not", "if", "get", "with", "keep", "that", "this", "every", "is", 'let', 'go');
-    // character 'a' is not included here it can be find in the middle 
-    if (isset($_GET['submit-search'])) {
-      $searchQ = htmlspecialchars($_GET['search-word']);
-      $res = str_ireplace($words_to_filter, "",   $searchQ);
-      $res = trim($res);
-      $res = explode(' ', $res);
-      // since a can be find in middle of the word 
-      foreach ($res as &$val) {
-        if ($val == 'a' || $val == 'A') $val = '';
-      }
-      $res = trim(implode('', $res));
-      $query = $res;
-      if (!empty($res)) {
+  // $words_to_filter = array("and", "not", "if", "get", "with", "keep", "that", "this", "every", "is", 'let', 'go');
+  // character 'a' is not included here it can be find in the middle 
+  if (isset($_GET['submit-search'])) {
+    $searchQ = htmlspecialchars($_GET['searchWord']);
+    // if ($in_array($searchQ, $words_to_filter)) {
 
-        $stmt = $db->prepare('SELECT * FROM products WHERE name LIKE :keywords');
-        $stmt->execute([
-          ':keywords' => '%' . $res . '%'
-        ]);
-        if ($stmt->rowCount()) {
-          while ($row = $stmt->fetch()) {
-            $id = $row['ID'];
-            $image = $row["image"];
-            $name = $row["name"];
-            $price = $row["price"];
-            $stock = $row["in_stock"];
-            $isOld = $row['is_old'];
+    //   echo $searchQ;
+    // }
+    // $res = str_ireplace($words_to_filter, "",   $searchQ);
+    $res = trim($searchQ);
+    $query = $res;
+    if (!empty($res)) {
 
-            $addToCartBtn = "<button class='feature-products__add' data-id=$id>ADD TO CART</button>";
-            if ($stock == 0) {
-                $addToCartBtn = "<div class='feature-products__oos'>OUT OF STOCK</div>";
-            }
-            if ($isOld == 1) {
-        
-        
-                $sale_price = $price * $salePercentage;
-                $sale = "
+      $stmt = $db->prepare('SELECT * FROM products WHERE name LIKE :keywords');
+      $stmt->execute([
+        ':keywords' => '%' . $res . '%'
+      ]);
+      if ($stmt->rowCount()) {
+        while ($row = $stmt->fetch()) {
+          $id = $row['ID'];
+          $image = $row["image"];
+          $name = $row["name"];
+          $price = $row["price"];
+          $stock = $row["in_stock"];
+          $isOld = $row['is_old'];
+
+          $addToCartBtn = "<button class='feature-products__add' data-id=$id>ADD TO CART</button>";
+          if ($stock == 0) {
+            $addToCartBtn = "<div class='feature-products__oos'>OUT OF STOCK</div>";
+          }
+          if ($isOld == 1) {
+
+
+            $sale_price = $price * $salePercentage;
+            $sale = "
                 <div class='feature-products__price'>
                     <span class='old-price'>$price SEK</span>
                     <span class='on-sale'>-10%</span>
                     <span class='new-price'>$sale_price SEK</span>
                 </div>";
-            }
-            else {
-                $sale = "<span class='old-price'>$price SEK</span>";
-            }
-        
-        
-        $results .= "<article class='feature-products__product'>
+          } else {
+            $sale = "<span class='old-price'>$price SEK</span>";
+          }
+
+
+          $results .= "<article class='feature-products__product'>
                 <a class='feature-products__link-wrap' href='./product.php?id=$id'>
                 <div class='feature-products__img-wrap'><img class='feature-products__img' src='./admin/images/$image' alt=''></div>
                 <div class='feature-products__product-title'>$name</div>
@@ -78,23 +75,23 @@
                 </a>
                 $addToCartBtn
               </article>";
-          }
-        } else {
-          $results = "<div>Nothing found for search term '$query'</div>";
         }
       } else {
         $results = "<div>Nothing found for search term '$query'</div>";
       }
+    } else {
+      $results = "<div>Nothing found for search term '$query'</div>";
     }
+  }
 
 
 
-    ?>
+  ?>
 
 
   <div class="search-results"><?= "Searched for: " . $query ?></div>
   <div class="search-container feature-products__product-wrap">
-    <?= $results?>
+    <?= $results ?>
   </div>
 
 
