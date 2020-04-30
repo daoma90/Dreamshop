374f02fa0b1b701532c95c76ed6d4b233753d8cb
David Öman <Daoma90@gmail.com>
2020-04-30 16:04:57 +0200

    changed search on mobile

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,25 +1,47 @@
-let header = document.querySelector('.header');
+let header = document.querySelector(".header");
 
 const words_to_filter = [
-  'and',
-  'not',
-  'if',
-  'get',
-  'with',
-  'keep',
-  'that',
-  'this',
-  'every',
-  'is',
-  'let',
-  'go',
+  "and",
+  "not",
+  "if",
+  "get",
+  "with",
+  "keep",
+  "that",
+  "this",
+  "every",
+  "is",
+  "let",
+  "go",
 ];
 function checkForm(form) {
   let search = form.searchWord.value;
   if (search.length <= 1 || words_to_filter.includes(search.trim())) {
     form.reset();
-    form.searchWord.placeholder = 'invalid Input';
+    form.searchWord.placeholder = "invalid Input";
     return false;
   }
   return true;
 }
+
+const searchInput = document.getElementsByClassName("header__search")[0];
+const logo = document.querySelector(".header__headline");
+let screenWidth = screen.width;
+
+window.addEventListener("resize", function () {
+  screenWidth = screen.width;
+});
+
+searchInput.addEventListener("focus", function () {
+  const logo = document.querySelector(".header__headline");
+
+  if (screenWidth < 724) {
+    logo.style.display = "none";
+  }
+});
+
+searchInput.addEventListener("focusout", function () {
+  if (screenWidth < 724) {
+    logo.style.display = "block";
+  }
+});
