d2534ba286fbe6cb0ef9d735febda885c9cba956
rongeee <44471095+rongeee@users.noreply.github.com>
2020-05-07 14:19:47 +0200

    rewrote confirmation.js (#5)

--- assets/js/confirmation.js
+++ assets/js/confirmation.js
@@ -1,41 +1,39 @@
-const total = document.getElementById("total").dataset.total;
-const productsContainer = document.querySelector(".confirmation-products");
+const cart = {
+  // Key should be randomized in a real project. Used as reference point and unique identifier
+  key: "qwerqwerqwerqwerqwerqwer",
+  products: [],
+};
 
+HappyLib.localStorageInit(cart.key);
+
+const total = HappyLib.getTotalPrice(cart.products);
+const productsContainer = document.querySelector(".confirmation-products");
 const id = document.getElementById("id").dataset.id;
 const productsTop = document.querySelector(".products-top");
+let wrapper = document.querySelector(".confirmation-products");
 
 productsTop.innerHTML += "<p class='products-id'>Order ID: " + id + "</p>";
 
-for (var i = 0; i < localStorage.length; i++) {
-  let key = localStorage.getItem(localStorage.key(i));
-  let value = JSON.parse(key);
+cart.products.forEach((item) => {
+  wrapper.innerHTML += '\n    <div class="product">\n      <div class="product__image"> <img src="../FE-Project-Shop/admin/images/'
+    .concat(
+      item.image,
+      '"></div>\n      <div class="product__info">\n        <div class="product__name">'
+    )
+    .concat(item.name, '</div>\n        <div class="product__price">')
+    .concat(
+      item.price,
+      ' EUR</div>\n        <div class="product__quantity">Quantity: '
+    )
+    .concat(item.quantity, "</div>\n      </div>\n    </div>");
+});
 
-  for (let i = 0; i < value.length; i++) {
-    const elm = value[i];
-    let wrapper = document.querySelector(".confirmation-products");
-    wrapper.innerHTML += '\n    <div class="product">\n      <div class="product__image"> <img src="../FE-Project-Shop/admin/images/'
-      .concat(
-        elm.image,
-        '"></div>\n      <div class="product__info">\n        <div class="product__name">'
-      )
-      .concat(elm.name, '</div>\n        <div class="product__price">')
-      .concat(
-        elm.price,
-        ' EUR</div>\n        <div class="product__quantity">Quantity: '
-      )
-      .concat(elm.quantity, "</div>\n      </div>\n    </div>");
-  }
-}
-// productsContainer.innerHTML +=
-//   '<div class="confirmation-products__total"><p class="confirmation-products__total-text">TOTAL</p>' +
-//   '<p class="confirmation-products__total-number">' +
-//   total +
-//   ' €' +
-//   '</p></div>';
 productsContainer.innerHTML +=
   '<div class="confirmation-products__total"><p class="confirmation-products__total-text">TOTAL</p>' +
   '<p class="confirmation-products__total-number">' +
   total +
   " €" +
   "</p></div>";
+
+cart.products = [];
 localStorage.clear();
