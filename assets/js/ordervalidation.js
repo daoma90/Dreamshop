c57120217264e66d928921aebddcec40261b0ebf
Samuel Mårtensson <samuel137@live.se>
2020-05-08 11:44:58 +0200

    added regex to order validation

--- assets/js/ordervalidation.js
+++ assets/js/ordervalidation.js
@@ -59,7 +59,12 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function validName(e) {
-    if (name.value.trim().length <= 1 || name.value.trim().length > 20) {
+    if (/[^a-zA-Z åäöÅÄÖ]/u.test(name.value)) {
+      error.textContent = 'Invalid characters';
+      error.style.visibility = 'visible';
+      error.style.color = 'red';
+      return false;
+    } else if (name.value.trim().length <= 1 || name.value.trim().length > 20) {
       error.textContent = 'Name must contain between 2-20 characters';
       error.style.visibility = 'visible';
       error.style.color = 'red';
@@ -83,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function validPhone(e) {
     mobileError.style.visibility = 'hidden';
     if (isNaN(mobile.value)) {
-      mobileError.textContent = 'Letters not allowed';
+      mobileError.textContent = 'Only digits allowed';
       mobileError.style.visibility = 'visible';
       mobileError.style.color = 'red';
       return false;
@@ -98,7 +103,12 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function validateAdress(e) {
-    if (adress.value.trim().length <= 3) {
+    if (/[^a-zA-Z0-9 åäöÅÄÖ]/u.test(adress.value)) {
+      adressError.textContent = 'Invalid characters';
+      adressError.style.visibility = 'visible';
+      adressError.style.color = 'red';
+      return false;
+    } else if (adress.value.trim().length <= 3) {
       adressError.textContent = 'Invalid address - too short';
       adressError.style.visibility = 'visible';
       adressError.style.color = 'red';
@@ -126,7 +136,15 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function validateCity(e) {
-    if (city.value.trim().length <= 3 || city.value.trim().length >= 20) {
+    if (/[^a-zA-Z åäöÅÄÖ]/u.test(city.value)) {
+      cityError.textContent = 'Invalid characters';
+      cityError.style.visibility = 'visible';
+      cityError.style.color = 'red';
+      return false;
+    } else if (
+      city.value.trim().length <= 3 ||
+      city.value.trim().length >= 20
+    ) {
       cityError.textContent = 'Invalid city - too short';
       cityError.style.visibility = 'visible';
       cityError.style.color = 'red';
